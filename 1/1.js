e6766775cbd718d48a8152779c905d697a5d90d3
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-06 01:30:58 +0330

    type of index checked

--- 1/1.js
+++ 1/1.js
@@ -1,33 +1,34 @@
-class Jack{
-    breakfast=new Date("2022 1 1 7:00:00 am")
-    lunch=new Date("2022 1 1 12:00:00 pm")
-    dinner=new Date("2022 1 1 7:00:00 pm")
-    nextEat(input){
-        var ans =0  
-        var mi=input.getHours()*60+input.getMinutes()
-        var mb= this.breakfast.getHours()*60+this.breakfast.getMinutes()
-        var ml= this.lunch.getHours()*60+this.lunch.getMinutes()
-        var md= this.dinner.getHours()*60+this.dinner.getMinutes()
-        if (input.getHours>7 || input.getHours()<12){
-            ans=ml-mi
-        }else if(input.getHours()>12||input.getHours()<19){
-            ans=md-mi
-        }else{
-            ans=mb-mi
+timeToEat=(str)=>{
+  const breakFast=7*60
+  const lunch=12*60
+  const dinner=19*60
+  const arrnum=[]
+ str.split("").map(input=>{
+    if(!(isNaN(input))){
+        arrnum.push(input)  
         }
-        this.secondsToHms(ans)   
-    }
-    secondsToHms(d) {
-        d = Number(d);
-        var h = Math.floor(d % 360 / 60);
-        var m = Math.floor(d % 360 % 60);
-        const ansarr=[]
-        ansarr.push(h,m)
-       console.log(ansarr) ; 
-    }
+    })
+    var hourInput =Number(arrnum[0]+arrnum[1])
+    if(str.charAt(6)==="p"){
+        hourInput+=12
+    } 
+    const minInput=hourInput*60 + Number(arrnum[2]+arrnum[3])
+    var answer=0
+    if (minInput>breakFast &&  minInput<lunch){
+         answer=lunch-minInput
+      }else if(minInput>lunch && minInput<dinner){
+         answer=dinner-minInput
+        }else{
+         answer=breakFast-minInput
+        }
+            var h = Math.floor(answer % 360 / 60);
+            var m = Math.floor(answer % 360 % 60);
+            const ansarr=[]
+            ansarr.push(h,m)
+           console.log(ansarr) ; 
 }
-let myobj=new Jack()
-let obj=new Date("2022 1 1 2:00:00 pm")
-let obj2=new Date("2022 1 1 5:50:00 pm")
-myobj.nextEat(obj2)
-myobj.nextEat(obj)
+
+timeToEat("05:50 pm")
+timeToEat("02:00 pm")
+
+
