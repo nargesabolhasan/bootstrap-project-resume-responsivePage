e12678741204b29e5d6828a849e13b905aeb499a
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-06 01:33:15 +0330

    time exchange to Hour

--- 1/1.js
+++ 1/1.js
@@ -8,6 +8,29 @@ timeToEat=(str)=>{
         arrnum.push(input)  
         }
     })
+    var hourInput =Number(arrnum[0]+arrnum[1])
+    if(str.charAt(6)==="p"){
+        hourInput+=12
+    } 
+    const minInput=hourInput*60 + Number(arrnum[2]+arrnum[3])
+    var answer=0
+    if (minInput>breakFast &&  minInput<lunch){
+         answer=lunch-minInput
+      }else if(minInput>lunch && minInput<dinner){
+         answer=dinner-minInput
+        }else{
+         answer=breakFast-minInput
+        }
+            var h = Math.floor(answer % 360 / 60);
+            var m = Math.floor(answer % 360 % 60);
+            const ansarr=[]
+            ansarr.push(h,m)
+           console.log(ansarr) ; 
+}
+
+timeToEat("05:50 pm")
+timeToEat("02:00 pm")
+
 
 
 
