0f726fcb9220705e34009c393d4e938a762cad8a
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-01-05 19:45:47 +0330

    all function created

--- 2/2.js
+++ 2/2.js
@@ -13,3 +13,40 @@ const stackCalculator = (str) => {
         opStack.push(input)
        }
     })
+    opStack.map(op=>{
+        switch(op){
+          case "+":sumFun(numStack.pop(),numStack.pop());
+              break;
+          case"-":subFun(numStack.pop(),numStack.pop());;
+              break;
+          case "*":mulFun(numStack.pop(),numStack.pop());
+              break;
+          case "/":divFun(numStack.pop(),numStack.pop());
+              break;
+          // case "D":dubFun(numStack.pop(),numStack.pop(),op)
+          }
+          function sumFun(a,b){
+            let ans=a+b
+            numStack.push(ans)
+        }
+        function subFun(a,b){
+            let ans=a-b
+            numStack.push(ans)
+            }
+        function mulFun(a,b){
+            let ans=a*b
+            numStack.push(ans)
+        }    
+        function divFun(a,b){
+            let ans=a/b
+            numStack.push(ans)
+            }
+        // function dubFun(a,b){
+        //     for(let i=0 ; i <opStack.lenght;i++,op){
+        //        if(op===opStack[i]){
+        //         opStack[i+1]
+        //        }
+        //     }
+        //     numStack.push(ans)
+        //     }        
+    
